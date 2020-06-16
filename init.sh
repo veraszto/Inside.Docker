08fe53d66ef054118ae4796930b8fd358343ebdb
Danilo G. Veraszto <garbini@gmail.com>
2020-06-16 11:15:06 -0300

    Tmux conf and Bashrc

---
+++ init.sh
@@ -0,0 +1,7 @@
+
+
+
+test ! -h $HOME/.bashrc && mv $HOME/.bashrc $HOME/dot.bashrc.original
+ln -s -f $HOME/git/Inside.Docker/dot.bashrc $HOME/.bashrc
+ln -s -f $HOME/git/Inside.Docker/dot.tmux.conf $HOME/.tmux.conf
+
