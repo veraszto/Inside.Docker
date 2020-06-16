c4bb6592b92fadd0b82ba1a5b52c6371b058b60c
Danilo G. Veraszto <garbini@gmail.com>
2020-06-16 17:18:42 -0300

    Vimrc

---
+++ dot.vimrc
@@ -0,0 +1,7 @@
+
+
+filetype plugin off
+filetype indent off
+
+set tabstop=4
+set smartindent
--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@
 test ! -h $HOME/.bashrc && mv $HOME/.bashrc $HOME/dot.bashrc.original
 ln -s -f $HOME/git/Inside.Docker/dot.bashrc $HOME/.bashrc
 ln -s -f $HOME/git/Inside.Docker/dot.tmux.conf $HOME/.tmux.conf
+ln -s -f $HOME/git/Inside.Docker/dot.vimrc $HOME/.vimrc
 
